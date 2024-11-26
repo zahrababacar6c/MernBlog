179cb215c61288af87baa97cd55a31d44b07f414
zahrababacar6c <zahrababacar6c@gmail.com>
2024-11-26 13:11:39 +0000

    Pages Creation

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,5 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import Dashboard from "./pages/Dashboard"
+import Signin from "./pages/signin"
+import Signup from "./pages/Signup"
+import Project from "./pages/Project"
+import About from "./pages/about"
+
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element= {<Home />} />
+        <Route path="/About" element= {<About />} />
+        <Route path="/dashboard" element= {<Dashboard />} />
+        <Route path="/signin" element= {<Signin />} />
+        <Route path="/signup" element= {<Signup />} />
+        <Route path="/project" element= {<Project />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
---
+++ vite-project/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+
+function Home() {
+  return (
+    <div>Home page
+         
+    </div>
+  )
+}
+
+export default Home
